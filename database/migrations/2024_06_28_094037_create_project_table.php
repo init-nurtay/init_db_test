0c28bc20d49c2a7127c2143a2a6bb7b086b564b6
init-nurtay <nurtay@init.red>
2024-06-28 14:58:21 +0500

    added migrations

---
+++ database/migrations/2024_06_28_094037_create_project_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Lead;
+use App\Models\PaymentFile;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('type')->nullable();
+            $table->string('status')->nullable();
+            $table->text('tech_file')->nullable();
+
+            $table->foreignIdFor(Lead::class)->nullable();
+//            $table->foreign('price_document_file')->references('id')->on('payment_files');
+            $table->timestamp('started_at')->nullable();
+            $table->timestamp('ended_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project');
+    }
+};
---
+++ database/migrations/2024_06_28_094158_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Project;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('status')->nullable();
+            $table->string('priority')->nullable();
+
+            $table->foreignIdFor(Project::class)->nullable();
+
+            $table->timestamp('started_at')->nullable();
+            $table->timestamp('ended_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
